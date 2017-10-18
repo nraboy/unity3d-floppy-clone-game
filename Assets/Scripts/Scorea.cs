0463e4f5de4036ea49c41dec242398461212eb46
ASHWANI KUMAR <ashwanidv100@gmail.com>
2017-10-18 19:18:04 +0530

    Create Scorea.cs

---
+++ Assets/Scripts/Scorea.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class Scorea : MonoBehaviour {
+	
+	// Use this for initialization
+	static public int score = 0;
+	void Start()
+	{
+		score = 0;
+	}
+	static public void AddPoint()
+	{
+		score++;
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		GetComponent<GUIText>().text = "SCORE: " + score;
+		PlayerPrefs.SetInt ("Score", score);
+	
+	}
+}
